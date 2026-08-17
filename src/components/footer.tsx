78cabe783e5d666c90c4594c5c8365c5f1710739
Omar <omarquinones977@gmail.com>
2026-08-16 17:38:27 -0700

    Updated footer color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,11 +4,11 @@ import "../styling/footer.css";
 const Footer = () => {
   return (
     <footer>
-      <span>
+      <span className="footer-text">
         © 2025 Beloved Care · <br className="br" /> Residential Care Facility
         for the Elderly · Anaheim, CA
       </span>
-      <span>
+      <span className="footer-text">
         Lic # 306006741 ·
         <a href="mailto:belovedcare.co@gmail.com">belovedcare.co@gmail.com</a>
       </span>
